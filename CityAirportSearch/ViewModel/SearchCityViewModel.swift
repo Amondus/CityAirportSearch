7ade4cc4241849d79794b5fa10459fa76725878f
Anton Zakharchenko <Zakharchenko.ant@gmail.com>
2020-10-18 14:41:30 +0300

    search and filter airports for tableView

--- CityAirportSearch/ViewModel/SearchCityViewModel.swift
+++ CityAirportSearch/ViewModel/SearchCityViewModel.swift
@@ -28,17 +28,52 @@ class SearchCityViewModel: SearchCityViewPresentable {
   private let airportService: AirportAPI
   private let disposeBag = DisposeBag()
   
+  typealias State = (airports: BehaviorRelay<Set<AirportModel>>, ())
+  private let state: State = (airports: BehaviorRelay(value: []), ())
+  
   init(input: SearchCityViewPresentable.Input,
        airportService: AirportAPI) {
     self.input = input
-    self.output = SearchCityViewModel.output(input: self.input)
+    self.output = SearchCityViewModel.output(input: self.input,
+                                             state: state,
+                                             disposeBag: disposeBag)
     self.airportService = airportService
     self.process()
   }
 }
 
 private extension SearchCityViewModel {
-  static func output(input: SearchCityViewPresentable.Input) -> SearchCityViewPresentable.Output {
+  static func output(input: SearchCityViewPresentable.Input,
+                     state: State,
+                     disposeBag: DisposeBag) -> SearchCityViewPresentable.Output {
+    
+    let searchTextObservable = input.searchText
+      .debounce(.milliseconds(300))
+      .distinctUntilChanged()
+      .skip(1)
+      .asObservable()
+      .share(replay: 1, scope: .whileConnected)
+    
+    let airportsObservable = state.airports
+      .skip(1)
+      .asObservable()
+    
+    Observable
+      .combineLatest(searchTextObservable, airportsObservable)
+      .map { searchKey, airports in
+        return airports.filter { airport -> Bool in
+          !searchKey.isEmpty &&
+            airport.city
+            .lowercased()
+            .replacingOccurrences(of: " ", with: "")
+            .hasPrefix(searchKey.lowercased())
+        }
+      }
+      .map {
+        print($0)
+      }
+      .subscribe()
+      .disposed(by: disposeBag)
     
     return ()
   }
@@ -46,8 +81,9 @@ private extension SearchCityViewModel {
   func process() {
     self.airportService
       .fetchAirports()
-      .map { airports in
-        print("Airports: \(airports)")
+      .map { Set($0)}
+      .map { [state] in
+        state.airports.accept($0)
       }
       .subscribe()
       .disposed(by: disposeBag)
